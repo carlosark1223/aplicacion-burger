e05d3a5fff58113ba0179c5908710f8d2d3ca4d2
Carlos Alcantar <carlosalcantar1223@gmail.com>
2019-12-17 17:52:46 -0800

    Seccion 17 334

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
-import * as actionTypes from '../../store/actions';
+import * as burgerBuilderActions from '../../store/actions/index';
 
 class BurgerBuilder extends Component {
     // constructor(props) {
@@ -24,13 +24,13 @@ class BurgerBuilder extends Component {
 
     componentDidMount () {
         console.log(this.props);
-        // axios.get( 'https://react-my-burger.firebaseio.com/ingredients.json' )
-        //     .then( response => {
-        //         this.setState( { ingredients: response.data } );
-        //     } )
-        //     .catch( error => {
-        //         this.setState( { error: true } );
-        //     } );
+        axios.get( 'https://react-my-burger.firebaseio.com/ingredients.json' )
+            .then( response => {
+                this.setState( { ingredients: response.data } );
+            } )
+            .catch( error => {
+                this.setState( { error: true } );
+            } );
     }
 
     updatePurchaseState ( ingredients ) {
@@ -109,8 +109,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
-        onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+        onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
     }
 }
 
---
+++ src/store/actions/burgerBuilder.js
@@ -0,0 +1,25 @@
+import * as actionTypes from './actionTypes';
+
+export const addIngredient = (name) => {
+    return {
+        type: actionTypes.ADD_INGREDIENT,
+        ingredientName: name
+    };
+};
+
+export const removeIngredient = (name) => {
+    return {
+        type: actionTypes.REMOVE_INGREDIENT,
+        ingredientName: name
+    };
+};
+
+export const setIngredients = (ingrdients) => {
+    
+};
+
+export const initIngredients = () => {
+    return dispatch => {
+
+    };
+};
